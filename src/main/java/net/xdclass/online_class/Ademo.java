540cff5f93947d7cfdf154a2808624892ca9d6ef
Administrator <sglgyz@163.com>
2020-08-11 14:03:59 +0800

    456

--- src/main/java/net/xdclass/online_class/Ademo.java
+++ src/main/java/net/xdclass/online_class/Ademo.java
@@ -7,6 +7,9 @@ public class Ademo {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
     }
 
 }
