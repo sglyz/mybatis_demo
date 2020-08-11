bb9b3e6ddc085b49436dddda876abab68bf3ce6c
Administrator <sglgyz@163.com>
2020-08-11 14:39:11 +0800

    sch

--- src/main/java/net/xdclass/online_class/Ademo.java
+++ src/main/java/net/xdclass/online_class/Ademo.java
@@ -5,56 +5,7 @@ public class Ademo {
     public static void main(String[] args) {
 
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-        System.out.println(6);
 
     }
 
-    public void  aa1 (){
-
-        System.out.println( 1111);
-
-    }
-    public void  aa2 (){
-
-        System.out.println( 1111);
-    }
-    public void  aa3 (){
-
-        System.out.println( 1111);
-    
-    }
-
-    public void aa4 (){
-
-        System.out.println("aa4");
-    }
-
-    public void aa5(){
-
-        System.out.println("aa5");
-    }
-
-
-
-
-
-
-
-
-
-    public  void aaaa(){
-
-
-        System.out.println("aabcccc");
-
-    }
-
-    public void abc(){
-
-        System.out.println(1111);
-    }
 }
