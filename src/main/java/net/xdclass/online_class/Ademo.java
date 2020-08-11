f7c0203866595463bb7f7a7ffbcd2bd7348f04ed
Administrator <sglgyz@163.com>
2020-08-11 14:07:44 +0800

    a1

--- src/main/java/net/xdclass/online_class/Ademo.java
+++ src/main/java/net/xdclass/online_class/Ademo.java
@@ -7,6 +7,23 @@ public class Ademo {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
+
+
     }
+
+    public void  aa3 (){
+
+        System.out.println( 1111);
+    }
+    public void  aa1 (){
+
+        System.out.println( 1111);
+    }
+    public void  aa2 (){
+
+        System.out.println( 1111);
+    
+    }
+
 
 }
